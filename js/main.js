08cefb9d3ca4675d456c7f7b7cbfe0b0fa622ced
Den Kh <icosagon@rambler.ru>
2020-06-22 21:00:21 +0300

    свайпер перекорячил и немного верстку

--- js/main.js
+++ js/main.js
@@ -1,31 +1,32 @@
 $( document ).ready(function() {
-
-  let swiper1 = new Swiper('.swiper-container', {
-    // Default parameters
-    slidesPerView: 1,
-    spaceBetween: 30,
-
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // Responsive breakpoints
-    breakpoints: {
-      // when window width is >= 480px
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 20
+  
+  
+    var swiperTeam = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // init: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
       },
-      // when window width is >= 640px
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 30
+      breakpoints: {
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
       }
-    }
-  })
+    });
 
 });
+
 
 
 // Modal Image Gallery
